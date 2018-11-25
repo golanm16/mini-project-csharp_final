29878cb2ccbd7aee8d85c0a889a291bf7f9c7dd5
golanm16 <45044888+golanm16@users.noreply.github.com>
2018-11-25 17:43:07 +0200

    started Test class definitions

--- MY_BE/Test.cs
+++ MY_BE/Test.cs
@@ -8,12 +8,26 @@ namespace MY_BE
 {
     class Test
     {
-        int _TestNumber;
+        int _TestNumber=0;
         public string TestNumber
         {
-            get => _TestNumber.ToString("D8");
-            
+            get { 
+                _TestNumber++;
+                _TestNumber.ToString("D8");
+                }
+        }
+        string TesterId;
+        string TraineeId;
+        DateTime TestDate;
+        DateTime TestDateTime;
+        Adress TestAdress;
+        bool TestParams;//make a lot of those!
+        //params for careful driving, stopping at stop sign,etc...
 
+        string TesterNote;
+        public string ToString()
+        {
+            return "";
         }
     }
 }
